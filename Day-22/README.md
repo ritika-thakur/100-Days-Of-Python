d5d3e5c8f540a274981bbe1c769d7f897fb47c1b
Ritika Thakur <ritikathakur17jan@gmail.com>
2021-06-12 21:34:27 +0530

    Added Files

---
+++ Day-22/README.md
@@ -0,0 +1,8 @@
+# Pong Game
+
+In today's project I have made a pong game using turtle library. Object oriented programming has been heavily.
+Go ahead and try playing the game yourself with your friends.
+
+The output of the code should be something like the gif below. 
+
+![alt text](https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5c19ac47973261.588a58667193c.gif)
---
+++ Day-22/paddle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from turtle import Turtle
+
+class Paddle(Turtle):
+    
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(5, 1)
+        self.penup()
+        self.goto(position)
+    
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+    
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+    
+
